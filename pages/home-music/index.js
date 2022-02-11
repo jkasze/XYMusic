6c0a0a661b6fc23fdba6cf1669c84c2b16f6c648
jkasze <jkasze@126.com>
2022-02-11 14:08:32 +0800

    音乐界面-巅峰榜的展示

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -25,34 +25,34 @@ Page({
    */
   onLoad: function (options) {
     // 获取页面数据
-    this.getPageData();
+    this.getPageData()
 
     // 发起共享数据的请求
-    rankingStore.dispatch("getRankingDataAction");
+    rankingStore.dispatch("getRankingDataAction")
 
     // 从store获取共享的数据
     rankingStore.onState("hotRanking", (res) => {
       if (!res.tracks) return;
       const recommendSongs = res.tracks.slice(0, 6);
       this.setData({ recommendSongs });
-    });
-    rankingStore.onState("newRanking", (res) => {});
-    rankingStore.onState("originRanking", (res) => {});
-    rankingStore.onState("upRanking", (res) => {});
+    })
+    rankingStore.onState("newRanking", this.getRankingHandler(0))
+    rankingStore.onState("originRanking",this.getRankingHandler(2))
+    rankingStore.onState("upRanking", this.getRankingHandler(3))
   },
 
   // 网络请求
   getPageData: function () {
     getBanners().then((res) => {
-      this.setData({ banners: res.banners });
-    });
+      this.setData({ banners: res.banners })
+    })
 
     getSongMenu().then((res) => {
-      this.setData({ hotSongMenu: res.playlists });
+      this.setData({ hotSongMenu: res.playlists })
     });
 
     getSongMenu("华语").then((res) => {
-      this.setData({ recommendSongMenu: res.playlists });
+      this.setData({ recommendSongMenu: res.playlists })
     });
   },
 
@@ -60,36 +60,36 @@ Page({
   handleSearchClick: function () {
     wx.navigateTo({
       url: "/pages/detail-search/index",
-    });
+    })
   },
 
   handleSwiperImageLoaded: function () {
     // 获取图片组件的高度
     throttleQueryRect(".swiper-image").then((res) => {
-      const rect = res[0];
-      this.setData({ swiperHeight: rect.height });
-    });
+      const rect = res[0]
+      this.setData({ swiperHeight: rect.height })
+    })
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    rankingStore.offState("newRanking");
+    rankingStore.offState("newRanking")
   },
 
   getRankingHandler: function (idx) {
     return (res) => {
-      if (Object.keys(res).length === 0) return;
-      const name = res.name;
-      const coverImgUrl = res.coverImgUrl;
-      const playCount = res.playCount;
-      const songList = res.tracks.slice(0, 3);
-      const rankingObj = { name, coverImgUrl, playCount, songList };
-      const newRankings = { ...this.data.rankings, [idx]: rankingObj };
+      if (Object.keys(res).length === 0) return
+      const name = res.name
+      const coverImgUrl = res.coverImgUrl
+      const playCount = res.playCount
+      const songList = res.tracks.slice(0, 3)
+      const rankingObj = { name, coverImgUrl, playCount, songList }
+      const newRankings = { ...this.data.rankings, [idx]: rankingObj }
       this.setData({
         rankings: newRankings,
-      });
-    };
+      })
+    }
   },
-});
+})
