83ce7ba7e854dd5ac915d783e00d0fc4af321a9a
jkasze <jkasze@126.com>
2022-02-12 20:19:40 +0800

    feat(home-music detail-songs song-menu-area api_music): 歌曲详情页-歌单数据获取和展示

--- pages/detail-songs/index.js
+++ pages/detail-songs/index.js
@@ -1,35 +1,46 @@
 // pages/detail-pages/index.js
-
 import { rankingStore } from '../../store/index'
-
+import { getSongMenuDetail } from '../../service/api_music'
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    type: "",
     ranking: "",
-    rankingInfo: {}
+    songInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const ranking = options.ranking
-    this.setData({ ranking })
+    const type = options.type
+    this.setData({ type })
+    if (type === "menu") {
+      const id = options.id
+      getSongMenuDetail(id).then(res => {
+        this.setData({ songInfo: res.playlist})
+      })
+    } else if (type === "rank") {
+      const ranking = options.ranking
+      this.setData({ ranking })
 
-    // 1.获取数据
-    rankingStore.onState(ranking, this.getRankingDataHanlder)
+      // 1.获取数据
+      rankingStore.onState(ranking, this.getRankingDataHanlder)
+    }
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    rankingStore.offState(this.data.ranking, this.getRankingDataHanlder)
+    if (this.data.ranking) {
+      rankingStore.offState(this.data.ranking, this.getRankingDataHanlder)
+    }
   },
 
   getRankingDataHanlder: function (res) {
-    this.setData({ rankingInfo: res })
+    this.setData({ songInfo: res })
   }
 })
