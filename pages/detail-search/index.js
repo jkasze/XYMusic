5308b3fdfa6b47c837b49beb50269f80b7dfa6ef
jkasze <jkasze@126.com>
2022-02-13 19:34:13 +0800

    feat(detail-search string2nodes):添加搜索匹配功能

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -1,6 +1,7 @@
 // pages/detail-search/index.js
 import { getSearchHot, getSearchSuggest } from '../../service/api_search'
 import debounce from '../../utils/debounce'
+import stringToNodes from '../../utils/string2nodes'
 
 const debounceGetSearchSuggest = debounce(getSearchSuggest, 300)
 
@@ -11,7 +12,8 @@ Page({
   data: {
     hotKeywords: [],
     suggestSongs: [],
-    searchValue: ""
+    suggestSongsNodes: [],
+    searchValue: ''
   },
 
   /**
@@ -23,23 +25,31 @@ Page({
   },
 
   // 网络请求
-  getPageData: function() {
-    getSearchHot().then(res => {
+  getPageData: function () {
+    getSearchHot().then((res) => {
       this.setData({ hotKeywords: res.result.hots })
     })
-    
   },
 
   // 事件处理
-  handleSearchChange: function(event) {
+  handleSearchChange: function (event) {
     const searchValue = event.detail
     this.setData({ searchValue })
     if (!searchValue.length) {
-      this.setData({ suggestSongs: []})
+      this.setData({ suggestSongs: [] })
       return
     }
-    debounceGetSearchSuggest(searchValue).then(res => {
+    debounceGetSearchSuggest(searchValue).then((res) => {
+      const suggestSongs = res.result.allMatch
       this.setData({ suggestSongs: res.result.allMatch })
+
+      const suggestKeywords = suggestSongs.map((item) => item.keyword)
+      const suggestSongsNodes = []
+      for (const keyword of suggestKeywords) {
+        const nodes = stringToNodes(keyword, searchValue)
+        suggestSongsNodes.push(nodes)
+      }
+      this.setData({ suggestSongsNodes })
     })
   }
 })
